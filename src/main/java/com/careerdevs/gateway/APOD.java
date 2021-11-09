1f09a11bda447e0cb3c1ced410d07bb6609edff9
TBlais10 <blaislighting@gmail.com>
2021-11-08 20:31:21 -0500

    Initial Commit

---
+++ src/main/java/com/careerdevs/gateway/APOD.java
@@ -0,0 +1,70 @@
+package com.careerdevs.gateway;
+
+public class APOD {
+
+    private String title;
+    private String date;
+    private String url;
+    private String explanation;
+    private String hdurl;
+    private String copyright;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    @Override
+    public String toString() {
+        return "APOD{" +
+                "title='" + title + '\'' +
+                ", date='" + date + '\'' +
+                ", url='" + url + '\'' +
+                ", explanation='" + explanation + '\'' +
+                ", hdurl='" + hdurl + '\'' +
+                ", copyright='" + copyright + '\'' +
+                '}';
+    }
+}
